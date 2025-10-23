895feeb3784d11c0fb16d09cc708ee668b011d2e
Filip <234220279+FichoLab@users.noreply.github.com>
2025-10-23 22:20:15 +0200

    Orbital velocity calculation
    Added gravitational parameter (μ) to planet dictionary and included orbital velocity calculation.

--- Orbiting-calculator.py
+++ Orbiting-calculator.py
@@ -4,20 +4,21 @@ G =  6.67430e-11  # Gravitational constant
 
 # AU is short for astronomical unit
 # 1 AU = 93,000,000 miles = 149,598,000 kilometers.
+# μ = M * G
 
-# Planet dictionary: (Planet name, Mass in kg, Radius in meters)
+# Planet dictionary: (Planet name, Mass in kg, Radius in meters, μ)
 planet = {
-    "1": ("Mercury", 3.30e23, 2.4397e6),
-    "2": ("Venus", 4.87e24, 6.0518e6),
-    "3": ("Earth", 5.97e24, 6.371e6),
-    "4": ("Mars", 6.39e23, 3.389e6),
-    "5": ("Jupiter", 1.90e27, 6.9911e7),
-    "6": ("Saturn", 5.68e26, 5.8232e7),
-    "7": ("Uranus", 8.68e25, 2.5362e7),
-    "8": ("Neptune", 1.02e26, 2.4622e7),
+    "1": ("Mercury",  3.30e23, 2.4397e6, 3.30e23 * G),
+    "2": ("Venus",    4.87e24, 6.0518e6, 4.87e24 * G),
+    "3": ("Earth",    5.97e24, 6.371e6,  5.97e24 * G),
+    "4": ("Mars",     6.39e23, 3.389e6,  6.39e23 * G),
+    "5": ("Jupiter",  1.90e27, 6.9911e7, 1.90e27 * G),
+    "6": ("Saturn",   5.68e26, 5.8232e7, 5.68e26 * G),
+    "7": ("Uranus",   8.68e25, 2.5362e7, 8.68e25 * G),
+    "8": ("Neptune",  1.02e26, 2.4622e7, 1.02e26 * G),
 
-    # For people who love Pluto so much
-    "9": ("Pluto", 1.31e22, 1.1883e6)
+    # Pluto lovers
+    "9": ("Pluto",    1.31e22, 1.1883e6, 1.31e22 * G)
 }
 
 # Minimum safe orbit altitudes (in km)
@@ -49,7 +50,7 @@ elif choice == "2":
     planet_choice = input("Enter planet number: ")
 
     if planet_choice in planet:
-        name, mass_planet, radius = planet[planet_choice]
+        name, mass_planet, radius, μ = planet[planet_choice]
 
         # Ask for orbit altitude
         altitude_km = float(input(f"Enter orbit altitude above {name} in kilometers (km): "))
@@ -72,3 +73,17 @@ elif choice == "2":
         print(f"{T/86400:.2f} days")
     else:
         print("Invalid planet selection.")
+
+    # Vis-Vira Equation (Orbital velocity) = Determines the velocity of an orbiter at any point in its orbit
+    # v = √[ μ (2/r - 1/a) ]
+
+    a = ((μ * T) / (4 * math.pi)) ** 1/3     # a = semi-major axis at the orbit
+
+    inside = μ * (2/radius - 1/a)
+    v = math.sqrt(inside)
+    print(f"Orbital velocity: {v:.2f} m/s")
+
+
+
+
+
